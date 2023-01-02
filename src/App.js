a9ef8f11d9a7f4b2ac7b92cf3db00549cf10aff2
Kevin <kevin.kipkoech@student.moringaschool.com>
2023-01-02 15:22:40 +0300

    feat: create new components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react"
+import SideBar from "./components/SideBar"
+import Editor from "./components/Editor"
+import Split from 'react-split'
+import { nanoid } from 'nanoid'
 
 export default function App() {
     return (
